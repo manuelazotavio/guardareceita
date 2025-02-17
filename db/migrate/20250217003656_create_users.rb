14ceaaaa6c690fc276e4d0f656f77a5ec4760387
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-02-16 21:38:49 -0300

    start project

---
+++ db/migrate/20250217003656_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.text :pass
+      t.text :avatar
+
+      t.timestamps
+
+
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
