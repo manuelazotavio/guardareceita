7c19d7aec05ac9f5a532e534b7d1220a5566a17b
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-02-26 13:39:54 -0300

    remove skkip verify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  # 
-  skip_before_action :verify_authenticity_token
+  #
   allow_browser versions: :modern
 end
